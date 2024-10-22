d8b42b73d5b66a991f497cc52169941e430a8e0d
lion92 <38532569+lion92@users.noreply.github.com>
2024-10-22 17:10:01 +0200

    toast inscription et connexion

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,47 +1,59 @@
 // src/pages/Login.jsx
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import Toast from "./Toast.jsx";
 
 function LoginPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-
+    const [toastMessage, setToastMessage] = useState('');
+    const [toastType, setToastType] = useState('');
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:3012/auth/login', { username, password });
+            const res = await axios.post('http://localhost:3012/auth/login', {username, password});
             const token = res.data.jwt;
 
             // Stocker le token JWT dans le localStorage
             localStorage.setItem('jwt', token);
 
-            alert('Connexion réussie');
+            setToastType("success")
+            setToastMessage("Connexion réussie")
+
             navigate('/recipes');  // Rediriger vers la page des recettes après la connexion
         } catch (error) {
             console.error(error);
-            alert('Échec de la connexion');
+            setToastType("error")
+            setToastMessage('Échec de la connexion')
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h2>Connexion</h2>
-            <input
-                type="text"
-                placeholder="Nom d'utilisateur"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+        <>
+            <form onSubmit={handleSubmit}>
+                <h2>Connexion</h2>
+                <input
+                    type="text"
+                    placeholder="Nom d'utilisateur"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Mot de passe"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Se connecter</button>
+            </form>
+            <Toast
+                message={toastMessage}
+                type={toastType}
+                onClose={() => setToastMessage('')}
             />
-            <input
-                type="password"
-                placeholder="Mot de passe"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Se connecter</button>
-        </form>
+        </>
     );
 }
 
--- src/SignupPage.jsx
+++ src/SignupPage.jsx
@@ -1,29 +1,45 @@
 // src/pages/Register.jsx
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import './css/inscription.css'
+import Toast from "./Toast.jsx";
+
 function SignupPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-
+    const [toastMessage, setToastMessage] = useState('');
+    const [toastType, setToastType] = useState('');
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post('http://localhost:3012/auth/register', { username, password });
-            alert('Inscription réussie');
+            await axios.post('http://localhost:3012/auth/register', {username, password});
+            setToastType("success")
+            setToastMessage("Inscription réussie")
         } catch (error) {
             console.error(error);
+            setToastType("error")
+            setToastMessage("Une erreur s'est produite.")
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h2>Inscription</h2>
-            <input type="text" placeholder="Nom d'utilisateur" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button type="submit">S'inscrire</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit}>
+                <h2>Inscription</h2>
+                <input type="text" placeholder="Nom d'utilisateur" value={username}
+                       onChange={(e) => setUsername(e.target.value)}/>
+                <input type="password" placeholder="Mot de passe" value={password}
+                       onChange={(e) => setPassword(e.target.value)}/>
+                <button type="submit">S'inscrire</button>
+            </form>
+            <Toast
+                message={toastMessage}
+                type={toastType}
+                onClose={() => setToastMessage('')}
+            />
+        </>
     );
+
 }
 
 export default SignupPage;
