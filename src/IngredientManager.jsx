5fb2ee3849f82906944649581e99c2a5bb0ea5ae
lion92 <38532569+lion92@users.noreply.github.com>
2024-10-22 16:53:49 +0200

    toast message pour ingredients

--- src/IngredientManager.jsx
+++ src/IngredientManager.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import useIngredientStore from './IngredientStore.jsx';
 import "./css/addRecette.css"
+import Toast from "./Toast.jsx";
+
 function IngredientManager() {
     // Récupération des actions et de l'état depuis le store Zustand
-    const { ingredients, fetchIngredients, addIngredient, updateIngredient, deleteIngredient } = useIngredientStore();
+    const {ingredients, fetchIngredients, addIngredient, updateIngredient, deleteIngredient} = useIngredientStore();
     const [newIngredient, setNewIngredient] = useState('');
     const [newIngredientPrice, setNewIngredientPrice] = useState(''); // État pour le prix
     const [editIngredientId, setEditIngredientId] = useState(null);
     const [editIngredientName, setEditIngredientName] = useState('');
     const [editIngredientPrice, setEditIngredientPrice] = useState(''); // État pour le prix en modification
+    const [toastMessage, setToastMessage] = useState('');
+    const [toastType, setToastType] = useState('');
 
     const token = localStorage.getItem('jwt'); // Récupérer le token JWT pour authentification
 
@@ -20,16 +24,26 @@ function IngredientManager() {
     // Fonction pour ajouter un nouvel ingrédient
     const handleAddIngredient = async () => {
         if (!newIngredient.trim()) {
-            alert('Le nom de l\'ingrédient ne peut pas être vide.');
+            setToastType("error")
+            setToastMessage('Le nom de l\'ingrédient ne peut pas être vide.')
             return;
         }
 
         if (!newIngredientPrice || parseFloat(newIngredientPrice) <= 0) {
-            alert('Le prix de l\'ingrédient doit être un nombre positif.');
+            setToastType("error")
+            setToastMessage('Le prix de l\'ingrédient doit être un nombre positif.')
             return;
         }
 
-        await addIngredient(newIngredient, parseFloat(newIngredientPrice), token); // Ajoute le prix
+        await addIngredient(newIngredient, parseFloat(newIngredientPrice), token).catch(() => {
+            setToastType("error")
+            setToastMessage('Une erreur s\'produite')
+        }).then(() => {
+            setToastType("success")
+            setToastMessage('Ingredient Ajouté')
+        }); // Ajoute le prix
+
+
         setNewIngredient(''); // Réinitialiser le champ de saisie
         setNewIngredientPrice(''); // Réinitialiser le champ de prix
     };
@@ -37,12 +51,15 @@ function IngredientManager() {
     // Fonction pour mettre à jour un ingrédient existant
     const handleUpdateIngredient = async (id) => {
         if (!editIngredientName.trim()) {
-            alert('Le nom de l\'ingrédient ne peut pas être vide.');
+
+            setToastType("error")
+            setToastMessage("Le nom de l\'ingrédient ne peut pas être vide.")
             return;
         }
 
         if (!editIngredientPrice || parseFloat(editIngredientPrice) <= 0) {
-            alert('Le prix de l\'ingrédient doit être un nombre positif.');
+            setToastType("error")
+            setToastMessage('Le prix de l\'ingrédient doit être un nombre positif.')
             return;
         }
 
@@ -58,23 +75,36 @@ function IngredientManager() {
     };
 
     return (
-        <div style={{maxWidth:500, margin:"auto",flexDirection:"column", display:"flex", alignItems:"center", justifyContent:"center"}}>
+        <div style={{
+            maxWidth: 500,
+            margin: "auto",
+            flexDirection: "column",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+        }}>
             <h2>Gestion des Ingrédients</h2>
 
-                <input
-                    type="text"
-                    value={newIngredient}
-                    onChange={(e) => setNewIngredient(e.target.value)}
-                    placeholder="Ajouter un ingrédient"
-                />
-                <input
-                    type="number"
-                    value={newIngredientPrice}
-                    onChange={(e) => setNewIngredientPrice(e.target.value)}
-                    placeholder="Prix"
-                />
-                <button onClick={handleAddIngredient}>Ajouter</button>
-            <ul style={{display:"flex", flexDirection:"column",alignItems:"center", justifyContent:"center", gap:2}}>
+            <input
+                type="text"
+                value={newIngredient}
+                onChange={(e) => setNewIngredient(e.target.value)}
+                placeholder="Ajouter un ingrédient"
+            />
+            <input
+                type="number"
+                value={newIngredientPrice}
+                onChange={(e) => setNewIngredientPrice(e.target.value)}
+                placeholder="Prix"
+            />
+            <button onClick={handleAddIngredient}>Ajouter</button>
+            <ul style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: 2
+            }}>
                 {Array.isArray(ingredients) && ingredients.length > 0 ? (
                     ingredients.map((ingredient) => (
                         <li key={ingredient.id}>
@@ -95,22 +125,28 @@ function IngredientManager() {
                                     </button>
                                 </div>
                             ) : (
-                                <div style={{display: "flex", alignItems: "center", justifyContent: "center", backgroundColor:"wheat", gap:1}}>
+                                <div style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    backgroundColor: "wheat",
+                                    gap: 1
+                                }}>
                                    <span>
                                        {ingredient.name} - {ingredient.price} €
                                    </span>
 
-                                        <button onClick={() => {
+                                    <button onClick={() => {
 
-                                            setEditIngredientId(ingredient.id);
-                                            setEditIngredientName(ingredient.name);
-                                            setEditIngredientPrice(ingredient.price);
-                                        }}>
-                                            Modifier
+                                        setEditIngredientId(ingredient.id);
+                                        setEditIngredientName(ingredient.name);
+                                        setEditIngredientPrice(ingredient.price);
+                                    }}>
+                                        Modifier
 
-                                        </button>
-                                        <button onClick={() => handleDeleteIngredient(ingredient.id)}>Supprimer</button>
-                                    </div>
+                                    </button>
+                                    <button onClick={() => handleDeleteIngredient(ingredient.id)}>Supprimer</button>
+                                </div>
                             )}
                         </li>
                     ))
@@ -119,7 +155,11 @@ function IngredientManager() {
                 )}
             </ul>
 
-
+            <Toast
+                message={toastMessage}
+                type={toastType}
+                onClose={() => setToastMessage('')}
+            />
         </div>
     );
 }
