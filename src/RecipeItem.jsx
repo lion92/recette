f0429e9899c0b9b60c0714f10c2f3d9082b89380
lion92 <38532569+lion92@users.noreply.github.com>
2024-10-30 18:49:40 +0100

    Reparation du delete de recette

--- src/RecipeItem.jsx
+++ src/RecipeItem.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import './css/recette.css';
 import useRecipeIdStore from "./RecipeIdStore.js";
-import {Button} from "@mui/material"; // Assurez-vous que le chemin est correct
+import {Button} from "@mui/material";
+import useRecipeStore from "./RecipeStore.js"; // Assurez-vous que le chemin est correct
 
 const RecipeItem = ({ recipe }) => {
     const { selectedRecipeId, selectRecipe } = useRecipeIdStore();
+
+    const {deleteRecipe}=useRecipeStore()
 
     const handleSelect = () => {
         console.log(recipe);
         selectRecipe(recipe.id); // Sélectionne la recette en utilisant son ID
     };
+    const handleDelete = async (recipeId) => {
+        const token = localStorage.getItem('jwt'); // Récupérer le token JWT
+        if (!token) {
+            alert('Vous devez être connecté pour supprimer une recette.');
+            return;
+        }
 
+        await deleteRecipe(recipeId, token);
+        alert('Recette supprimée avec succès !');
+    };
     return (
         <div className="card" onClick={handleSelect}>
-            <Button color="primary" onClick={() => deleteRecipe(recipe.id, ""+localStorage.getItem('jwt'))}>Supprimer</Button>
+            <Button color="primary" onClick={() => handleDelete(recipe.id, ""+localStorage.getItem('jwt'))}>Supprimer</Button>
             <div className="card--image-wrapper">
                 <div className="card--view">
                     <p>View Recipe</p>
