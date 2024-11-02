ba3d8d094eea1b9d15491cc6510ab9ce6905633b
lion92 <38532569+lion92@users.noreply.github.com>
2024-11-02 10:50:07 +0100

    menu sans background

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Button, Box, IconButton, Drawer, List, ListItem, ListItemText } from '@mui/material';
+import { AppBar, Toolbar, Button, Box, IconButton, Drawer, List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import '../src/css/menu.css'
+import LoginIcon from '@mui/icons-material/Login'; // Exemple d'icône
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import CategoryIcon from '@mui/icons-material/Category';
+import IngredientIcon from '@mui/icons-material/Restaurant';
+import RecipeIcon from '@mui/icons-material/MenuBook';
+import CreateIcon from '@mui/icons-material/Create';
+import ProfileIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import '../src/css/menu.css';
+
 function Menu() {
     const [drawerOpen, setDrawerOpen] = useState(false);
 
@@ -14,29 +23,30 @@ function Menu() {
         setDrawerOpen(open);
     };
 
+    // Liste des items avec texte et icônes
     const menuItems = [
-        { text: 'Connexion', to: '/login' },
-        { text: 'Inscription', to: '/signup' },
-        { text: 'Catégorie de Recette', to: '/addCategory' },
-        { text: 'Ingrédient', to: '/addIngredient' },
-        { text: 'Recettes', to: '/' },
-        { text: 'Créer une Recette', to: '/createRecette' },
-        { text: 'Profil', to: '/profil' },
-        { text: 'Déconnexion', to: '/logout' },
+        { text: 'Connexion', to: '/login', icon: <LoginIcon /> },
+        { text: 'Inscription', to: '/signup', icon: <PersonAddIcon /> },
+        { text: 'Catégorie de Recette', to: '/addCategory', icon: <CategoryIcon /> },
+        { text: 'Ingrédient', to: '/addIngredient', icon: <IngredientIcon /> },
+        { text: 'Recettes', to: '/', icon: <RecipeIcon /> },
+        { text: 'Créer une Recette', to: '/createRecette', icon: <CreateIcon /> },
+        { text: 'Profil', to: '/profil', icon: <ProfileIcon /> },
+        { text: 'Déconnexion', to: '/logout', icon: <LogoutIcon /> },
     ];
 
     const drawerList = () => (
         <Box
-            sx={{width: 250}}
+            sx={{ width: 250 }}
             role="presentation"
             onClick={toggleDrawer(false)}
             onKeyDown={toggleDrawer(false)}
         >
-
             <List>
                 {menuItems.map((item) => (
                     <ListItem button key={item.text} component={Link} to={item.to}>
-                        <ListItemText primary={item.text}/>
+                        <ListItemIcon>{item.icon}</ListItemIcon> {/* Ajoutez l'icône ici */}
+                        <ListItemText primary={item.text} />
                     </ListItem>
                 ))}
             </List>
@@ -45,18 +55,17 @@ function Menu() {
 
     return (
         <div>
-            <AppBar position="static" color="primary">
-                <div className="menu-logo">
-                </div>
+            <AppBar position="static" color="primary" sx={{background:'none'}}>
+                <div className="menu-logo"></div>
                 <Toolbar>
-                    {/* Icone du menu pour les petits écrans */}
+                    {/* Icône du menu pour les petits écrans */}
                     <IconButton
                         color="inherit"
                         aria-label="menu"
-                        sx={{display: {xs: 'block', md: 'none'}}}
+                        sx={{ display: { xs: 'block', md: 'none' } }}
                         onClick={toggleDrawer(true)}
                     >
-                        <MenuIcon/>
+                        <MenuIcon />
                     </IconButton>
 
                     {/* Menu pour les grands écrans */}
@@ -64,7 +73,7 @@ function Menu() {
                         display="flex"
                         justifyContent="center"
                         flexGrow={1}
-                        sx={{display: {xs: 'none', md: 'flex', marginTop: "50px"}}}
+                        sx={{ display: { xs: 'none', md: 'flex', marginTop: '50px' } }}
                     >
                         {menuItems.map((item) => (
                             <Button
@@ -72,6 +81,7 @@ function Menu() {
                                 color="inherit"
                                 component={Link}
                                 to={item.to}
+                                startIcon={item.icon} // Ajoutez l'icône ici
                             >
                                 {item.text}
                             </Button>
@@ -81,11 +91,7 @@ function Menu() {
             </AppBar>
 
             {/* Drawer pour les petits écrans */}
-            <Drawer
-                anchor="left"
-                open={drawerOpen}
-                onClose={toggleDrawer(false)}
-            >
+            <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
                 {drawerList()}
             </Drawer>
         </div>
