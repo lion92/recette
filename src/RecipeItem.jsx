856db5b1f8984d54b1818cddd0270de63c844637
lion92 <38532569+lion92@users.noreply.github.com>
2024-11-06 18:55:56 +0100

    Faire mieux l'affichage des recettes dans une modale

--- src/RecipeItem.jsx
+++ src/RecipeItem.jsx
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './css/recette.css';
-import useRecipeIdStore from './RecipeIdStore.js';
 import {
     Button,
     FormControl,
     InputLabel,
     MenuItem,
     Select,
-    TextField
+    TextField,
+    Modal,
+    Box
 } from '@mui/material';
 import useRecipeStore from './RecipeStore.js';
+import useIngredientStore from "../src/IngredientStore.jsx";
+import useCategoryStore from "../src/UseCategoryStore.js";
 import Toast from './Toast.jsx';
-import useIngredientStore from "../src/IngredientStore.jsx"; // Vérifiez le chemin correct
-import useCategoryStore from "../src/UseCategoryStore.js"; // Vérifiez le chemin correct
 
 const RecipeItem = ({ recipe }) => {
-    const { selectRecipe, updateRecipe, deleteRecipe, fetchRecipes } = useRecipeStore();
+    const { updateRecipe, deleteRecipe, fetchRecipes } = useRecipeStore();
     const { ingredients, fetchIngredients } = useIngredientStore();
     const { categories, fetchCategories } = useCategoryStore();
 
@@ -23,15 +24,15 @@ const RecipeItem = ({ recipe }) => {
     const [toastType, setToastType] = useState('');
     const [isEditing, setIsEditing] = useState(false);
     const [updatedRecipe, setUpdatedRecipe] = useState(recipe);
+    const [openModal, setOpenModal] = useState(false);
 
     useEffect(() => {
         fetchIngredients();
         fetchCategories();
     }, [fetchIngredients, fetchCategories]);
 
-    const handleSelect = () => {
-        selectRecipe(recipe.id);
-    };
+    const handleModalOpen = () => setOpenModal(true);
+    const handleModalClose = () => setOpenModal(false);
 
     const handleDelete = async (recipeId) => {
         const token = localStorage.getItem('jwt');
@@ -45,11 +46,11 @@ const RecipeItem = ({ recipe }) => {
             await deleteRecipe(recipeId, token);
             setToastType('success');
             setToastMessage('Recette supprimée avec succès !');
-            await fetchRecipes(); // Actualise la liste des recettes après la suppression
+            await fetchRecipes();
+            handleModalClose();
         } catch (error) {
             setToastType('error');
             setToastMessage("Une erreur s'est produite lors de la suppression de la recette.");
-            console.error('Erreur lors de la suppression de la recette:', error);
         }
     };
 
@@ -66,11 +67,10 @@ const RecipeItem = ({ recipe }) => {
             setToastType('success');
             setToastMessage('Recette mise à jour avec succès !');
             setIsEditing(false);
-            await fetchRecipes(); // Actualise la liste des recettes après la mise à jour
+            await fetchRecipes();
         } catch (error) {
             setToastType('error');
             setToastMessage("Une erreur s'est produite lors de la mise à jour de la recette.");
-            console.error('Erreur lors de la mise à jour de la recette:', error);
         }
     };
 
@@ -82,164 +82,109 @@ const RecipeItem = ({ recipe }) => {
         setUpdatedRecipe({ ...updatedRecipe, categories: event.target.value });
     };
 
-    const addIngredient = () => {
-        setUpdatedRecipe({
-            ...updatedRecipe,
-            ingredients: [...(updatedRecipe.ingredients || []), { id: null, name: 'Nouvel Ingrédient' }]
-        });
-    };
-
-    const removeIngredient = (index) => {
-        // Supprime l'ingrédient de la liste locale
-        const newIngredients = [...updatedRecipe.ingredients];
-        newIngredients.splice(index, 1);
-        setUpdatedRecipe({ ...updatedRecipe, ingredients: newIngredients });
-    };
-
-    const addCategory = () => {
-        setUpdatedRecipe({
-            ...updatedRecipe,
-            categories: [...(updatedRecipe.categories || []), { id: null, name: 'Nouvelle Catégorie' }]
-        });
-    };
-
-    const removeCategory = (index) => {
-        // Supprime la catégorie de la liste locale
-        const newCategories = [...updatedRecipe.categories];
-        newCategories.splice(index, 1);
-        setUpdatedRecipe({ ...updatedRecipe, categories: newCategories });
-    };
-
     return (
-        <div className="card" onClick={handleSelect}>
-            <Button
-                color="primary"
-                onClick={(e) => {
-                    e.stopPropagation();
-                    handleDelete(recipe.id);
-                }}
-            >
-                Supprimer
-            </Button>
-            <Button
-                color="secondary"
-                onClick={(e) => {
-                    e.stopPropagation();
-                    setIsEditing(!isEditing);
-                }}
-            >
-                {isEditing ? 'Annuler' : 'Modifier'}
-            </Button>
-            <div className="card--image-wrapper">
+        <div className="card">
+            <div className="card--image-wrapper" onClick={handleModalOpen}>
                 <div className="card--view">
                     <p>Voir la recette</p>
                 </div>
                 <div className="card--image"></div>
             </div>
-            <div className="card--content-wrapper">
-                {isEditing ? (
-                    <>
-                        <TextField
-                            label="Titre"
-                            fullWidth
-                            margin="normal"
-                            value={updatedRecipe.title}
-                            onChange={(e) => setUpdatedRecipe({ ...updatedRecipe, title: e.target.value })}
-                        />
-                        <TextField
-                            label="Description"
-                            fullWidth
-                            margin="normal"
-                            value={updatedRecipe.description}
-                            onChange={(e) => setUpdatedRecipe({ ...updatedRecipe, description: e.target.value })}
-                        />
-                        <TextField
-                            label="Instructions"
-                            fullWidth
-                            margin="normal"
-                            value={updatedRecipe.instructions}
-                            onChange={(e) => setUpdatedRecipe({ ...updatedRecipe, instructions: e.target.value })}
-                        />
-                        <FormControl fullWidth margin="normal">
-                            <InputLabel>Ingrédients</InputLabel>
-                            <Select
-                                multiple
-                                value={updatedRecipe.ingredients || []}
-                                onChange={handleIngredientChange}
-                                renderValue={(selected) => selected.map((ingredient) => ingredient.name).join(', ')}
-                            >
-                                {ingredients.map((ingredient) => (
-                                    <MenuItem key={ingredient.id} value={ingredient}>
-                                        {ingredient.name}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                        <ul>
-                            {updatedRecipe.ingredients && updatedRecipe.ingredients.length > 0 ? (
-                                updatedRecipe.ingredients.map((ingredient, index) => (
-                                    <li key={index}>
-                                        {ingredient.name}
-                                        <Button onClick={() => removeIngredient(index)}>Supprimer</Button>
-                                    </li>
-                                ))
-                            ) : (
-                                <li>Aucun ingrédient spécifié</li>
-                            )}
-                        </ul>
-                        <FormControl fullWidth margin="normal">
-                            <InputLabel>Catégories</InputLabel>
-                            <Select
-                                multiple
-                                value={updatedRecipe.categories || []}
-                                onChange={handleCategoryChange}
-                                renderValue={(selected) => selected.map((category) => category.name).join(', ')}
-                            >
-                                {categories.map((category) => (
-                                    <MenuItem key={category.id} value={category}>
-                                        {category.name}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                        <ul>
-                            {updatedRecipe.categories && updatedRecipe.categories.length > 0 ? (
-                                updatedRecipe.categories.map((category, index) => (
-                                    <li key={index}>
-                                        {category.name}
-                                        <Button onClick={() => removeCategory(index)}>Supprimer</Button>
-                                    </li>
-                                ))
-                            ) : (
-                                <li>Aucune catégorie spécifiée</li>
-                            )}
-                        </ul>
-                        <Button onClick={handleUpdate} color="primary">
-                            Enregistrer
+
+            <Modal open={openModal} onClose={handleModalClose}>
+                <Box className="modal-content">
+                    <div className="modal-header">
+                        <Button color="primary" onClick={() => setIsEditing(!isEditing)}>
+                            {isEditing ? 'Annuler' : 'Modifier'}
                         </Button>
-                    </>
-                ) : (
-                    <>
-                        <h1 className="card--title">{recipe.title}</h1>
-                        <p className="card--description">{recipe.description}</p>
-                        <p className="card--description">{recipe.instructions}</p>
-                        <p>Prix total : {recipe.totalCost} €</p>
-                        <p>Calories totales : {(recipe.totalCalories / 100).toFixed(2)} kCal</p>
-                        <h5>Ingrédients :</h5>
-                        <ul>
-                            {recipe.ingredients.map((ingredient, index) => (
-                                <li key={index}>{ingredient.name} - {ingredient.price} €</li>
-                            ))}
-                        </ul>
-                        <h5>Catégories :</h5>
-                        <ul>
-                            {recipe.categories.map((category, index) => (
-                                <li key={index}>{category.name}</li>
-                            ))}
-                        </ul>
-                    </>
-                )}
-            </div>
+                        <Button color="secondary" onClick={() => handleDelete(recipe.id)}>
+                            Supprimer
+                        </Button>
+                    </div>
+
+                    <div className="modal-body">
+                        {isEditing ? (
+                            <>
+                                <TextField
+                                    label="Titre"
+                                    fullWidth
+                                    margin="normal"
+                                    value={updatedRecipe.title}
+                                    onChange={(e) => setUpdatedRecipe({ ...updatedRecipe, title: e.target.value })}
+                                />
+                                <TextField
+                                    label="Description"
+                                    fullWidth
+                                    margin="normal"
+                                    value={updatedRecipe.description}
+                                    onChange={(e) => setUpdatedRecipe({ ...updatedRecipe, description: e.target.value })}
+                                />
+                                <TextField
+                                    label="Instructions"
+                                    fullWidth
+                                    margin="normal"
+                                    value={updatedRecipe.instructions}
+                                    onChange={(e) => setUpdatedRecipe({ ...updatedRecipe, instructions: e.target.value })}
+                                />
+                                <FormControl fullWidth margin="normal">
+                                    <InputLabel>Ingrédients</InputLabel>
+                                    <Select
+                                        multiple
+                                        value={updatedRecipe.ingredients || []}
+                                        onChange={handleIngredientChange}
+                                        renderValue={(selected) => selected.map((ingredient) => ingredient.name).join(', ')}
+                                    >
+                                        {ingredients.map((ingredient) => (
+                                            <MenuItem key={ingredient.id} value={ingredient}>
+                                                {ingredient.name}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                                <FormControl fullWidth margin="normal">
+                                    <InputLabel>Catégories</InputLabel>
+                                    <Select
+                                        multiple
+                                        value={updatedRecipe.categories || []}
+                                        onChange={handleCategoryChange}
+                                        renderValue={(selected) => selected.map((category) => category.name).join(', ')}
+                                    >
+                                        {categories.map((category) => (
+                                            <MenuItem key={category.id} value={category}>
+                                                {category.name}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                                <Button onClick={handleUpdate} color="primary">
+                                    Enregistrer
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <h1>{recipe?.title}</h1>
+                                <h1>{recipe?.user?.email}</h1>
+                                <p>{recipe?.description}</p>
+                                <p>{recipe?.instructions}</p>
+                                <p>Prix total : {recipe.totalCost} €</p>
+                                <p>Calories totales : {(recipe.totalCalories / 100).toFixed(2)} kCal</p>
+                                <h5>Ingrédients :</h5>
+                                <ul>
+                                    {recipe.ingredients.map((ingredient, index) => (
+                                        <li key={index}>{ingredient.name} - {ingredient.price} €</li>
+                                    ))}
+                                </ul>
+                                <h5>Catégories :</h5>
+                                <ul>
+                                    {recipe.categories.map((category, index) => (
+                                        <li key={index}>{category.name}</li>
+                                    ))}
+                                </ul>
+                            </>
+                        )}
+                    </div>
+                </Box>
+            </Modal>
 
             <Toast
                 message={toastMessage}
